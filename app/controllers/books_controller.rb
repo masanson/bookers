80f09ebb5741ec6521597d22b1a19f15852439ec
masanson <138214683+masanson@users.noreply.github.com>
2023-07-17 11:14:52 +0000

    [Update]課題2修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,13 @@
 class BooksController < ApplicationController
-
-  def new
-    @book = Book.new
-  end
-
+  
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
